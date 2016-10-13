9446f976f8ec8842a5745982098e7bcd082c9335
hansellesteban <hansell.ramos@gmail.com>
2016-10-12 19:02:04 -0500

    #FirstCommit

--- NubeSemana1/ViewController.swift
+++ NubeSemana1/ViewController.swift
@@ -12,12 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        asincrono()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func sincrono() -> Void {
+        let urls = "http://dia.ccm.itesm.mx"
+        let url = URL(string: urls)
+        let datos:NSData? = NSData(contentsOf: url!)
+        let texto = NSString(data: datos! as Data, encoding: String.Encoding.utf8.rawValue)
+        print(texto!)
+    }
+    
+    func asincrono() -> Void {
+        let urls = "http://dia.ccm.itesm.mx"
+        let url = URL(string: urls)
+        let session = URLSession.shared
+        let bloque = {
+            (datos: Data?, resp: URLResponse?, error:Error?) -> Void in
+                let texto = NSString(data: datos! as Data, encoding:String.Encoding.utf8.rawValue)
+                print(texto)
+        }
+        let dt = session.dataTask(with: url!, completionHandler: bloque)
+        dt.resume()
+        print("Antes o despues")
     }
 
 
